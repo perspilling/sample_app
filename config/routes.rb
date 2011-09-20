1d4a72c18097dd1cc29c15d1b157b04d11653704
perspilling <per@kodemaker.no>
2011-09-20 13:07:31 +0200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "issues/rspec_title_problem"
+  # basic routes
+  get "users/new"
 
   get "pages/home"
   get "pages/contact"
   get "pages/about"
 
+  get "issues/rspec_title_problem"
+
+  # aliases
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
   root :to => 'pages#home'
 
